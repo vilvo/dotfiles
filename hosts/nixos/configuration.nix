1327ba0d338d28c725aef04466352c47f7ae67bd
Ville Ilvonen <vilvo@iki.fi>
2022-09-07 09:07:37 +0300

    enable audio
    Signed-off-by: Ville Ilvonen <vilvo@iki.fi>

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -34,6 +34,15 @@
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
   nix = {
     settings = {
       auto-optimise-store = true;
