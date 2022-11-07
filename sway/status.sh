e8e9f0e81808c538f0c2d2b7abc4d88016839985
Ville Ilvonen <vilvo@iki.fi>
2022-11-07 14:16:38 +0200

    minor formatting to status bar + update
    Signed-off-by: Ville Ilvonen <vilvo@iki.fi>

--- sway/status.sh
+++ sway/status.sh
@@ -3,4 +3,4 @@
 date_formatted=$(date +'%Y-%m-%d %H:%M')
 battery_status=$(cat /sys/class/power_supply/BAT0/capacity)
 
-echo $battery_status 🔋 $date_formatted
+echo $battery_status% 🔋 $date_formatted
