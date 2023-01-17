d44262d778f2a69a5038e59c304e6f52e2fa2ca7
Ville Ilvonen <vilvo@iki.fi>
2023-01-17 09:05:27 +0200

    restructure: blip/blop differences
    * nouveau drivers on blop for sway
    * enable opengl on blop
    * sway does not start with:
      - EGL_NOT_INITIALIZED (0x3001)
    * enable ssh server on blop, authorized key
    
    Signed-off-by: Ville Ilvonen <vilvo@iki.fi>

--- hosts/nixos/blop/default.nix
+++ hosts/nixos/blop/default.nix
@@ -6,6 +6,19 @@
   imports = [ (import ./hardware-configuration.nix) ]
     ++ [ (import ./networking.nix) ];
 
+  services.xserver.videoDrivers = [ "nouveau" ];
+  hardware.opengl.enable = true;
+
+  users.users.${user} = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAS51BVp5fecNMh2zm72H3j8qtzW3JnWYxaw/ejQOvnX vilvo@blip"
+    ];
+  };
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "22.11";
+
   # Bootloader.
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
