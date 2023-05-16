48678a22f9618fae7ee7d22b24f9f5660daee2e1
Ville Ilvonen <ville.ilvonen@unikie.com>
2023-05-16 12:50:37 +0300

    nvidia drivers, m1 as remote builder
    * nouveau does not work with sway
    * nvidia works with glitches using --unsupported-gpu
      (pending a radeon card)
    * disable stale display setting in hardware
    * m1 as remote builder in the office network
    
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- hosts/nixos/blop/default.nix
+++ hosts/nixos/blop/default.nix
@@ -6,14 +6,41 @@
   imports = [ (import ./hardware-configuration.nix) ]
     ++ [ (import ./networking.nix) ];
 
-  services.xserver.videoDrivers = [ "nouveau" ];
+  environment.systemPackages = with pkgs; [ linux-firmware foot ];
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
+  hardware.nvidia.modesetting.enable = true;
 
   users.users.${user} = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAS51BVp5fecNMh2zm72H3j8qtzW3JnWYxaw/ejQOvnX vilvo@blip"
     ];
   };
+
+  nix = {
+    buildMachines = [
+    {
+      hostName = "172.18.8.65";
+      system = "aarch64-linux";
+      maxJobs = 1;
+      sshUser = "vilvo";
+      supportedFeatures = [ "kvm" "benchmark" "big-parallel" "nixos-test" ];
+      sshKey = "/home/vilvo/.ssh/m1_nixos";
+      publicHostKey = "AAAAC3NzaC1lZDI1NTE5AAAAIJbtUwpDSCLntv2ePOlwrpFeMkXSVrT76umd4rf5LGai";
+    }
+    ];
+  };
+
+  # nix remote builders also require root accepted host key to avoid:
+  # "Host key verification failed"
+  # see https://github.com/NixOS/nix/issues/2030#issuecomment-1289522180
+  # following is the output of: $ ssh-keyscan 192.168.1.174
+  environment.etc."ssh/ssh_known_hosts".text = ''
+    172.18.8.65 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1hZQT82XIfNiw2yaH1sFlko6ZWrrafLN5EPQ4g1ILNdoqvWcNdiUUEWCyST2t+ekLSpV05HWHD2vq7LsyWK7M6hfDLlQswFT5KSSKkia5DvlGwZCV1Z6HTgXmN+Pscj8grasZ5OnD+zCFVu9Tj3ReRSJyTEpipvsGBILPBVWke1X9tXcLORPeLKCGSrV4C85xT6Ufioheua/XzkpDaNNdCtj90NgmLd+JfWdoArN3CRAvODyero36f6z4ojIhiDGZUfKf2wR5GkMgRQkW/vyvNAsGbuAj08N4WbXoerYYZv+mdOPFwTzMikQ7f9Y44NZvmsvGxhPsbix7Qadc3dicBVpcFFYEysIk60mtJ7UdvtWX3FpPawCtTtUdJ3S/f5M3ugUw+hQZQi8okYaOhdZ1sthKMcmuxCK2zqHmWekJnC7wp7oLUp9cnTwprRKnzsD+U6Y1GE1Bmnd/hCduheL2eRe5hg6vpkv7vIsh7YtEA3l83+htQEF4YXkzU5poxqhxUR6i7e35QpmffLITzLXHDeCdC5TjtPOCplj6yWR/dzwTqoj1JWV5F2pSQxwK0uKYqCR9myVsMDPGzDeqysCSB5WF6oA9keEFW9vHN8x55FL+zWUpDXBYWn8mNNeNZ6LB0Wr+S2KIXt9uAp+OYCtcEYBQ8xbKkk84iNCRpMcgew==
+    172.18.8.65 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbtUwpDSCLntv2ePOlwrpFeMkXSVrT76umd4rf5LGai
+    '';
 
   services.openssh.enable = true;
 
