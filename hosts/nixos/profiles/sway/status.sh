b80e0b739fa95f8b6dc7bc1f59a8c3ead51100d8
Ville Ilvonen <ville.ilvonen@unikie.com>
2023-10-04 17:18:17 +0300

    battery: show per host
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- hosts/nixos/profiles/sway/status.sh
+++ hosts/nixos/profiles/sway/status.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 date_formatted=$(date +'%Y-%m-%d %H:%M')
-battery_status=$(cat /sys/class/power_supply/BAT0/capacity)
+host=$(hostname)
+
+# the default applies to desktop - always powered
+battery_status="100"
+
+if [[ $host = "blip" ]] || [[ $host = "carrie" ]]
+then
+    battery_status=$(cat /sys/class/power_supply/BAT0/capacity)
+elif [[ $host = "blub" ]]
+then
+    battery_status=$(cat /sys/class/power_supply/macsmc-battery/capacity)
+fi
 
 echo $battery_status% 🔋 $date_formatted
