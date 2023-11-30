34f1cb8af3261c5c5f13466a417bba103d9674e3
Ville Ilvonen <ville.ilvonen@unikie.com>
2023-11-30 13:41:48 +0200

    blub: ssh authorized key
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- hosts/nixos/blub/default.nix
+++ hosts/nixos/blub/default.nix
@@ -9,5 +9,11 @@
 
   networking.hostName = "blub";
 
+  users.users.${user} = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwRelxFwR6WPBm86b52q7pjQd8mEiqj1R3yj6YVL6wM vilvo@blip"
+    ];
+  };
+
   system.stateVersion = "23.05";
 }
