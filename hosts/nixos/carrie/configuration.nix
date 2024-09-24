655eaa5f894b58a077058caf0a9e319477810e62
Ville Ilvonen <vilvo@iki.fi>
2024-09-24 13:02:16 +0300

    update: carrie + binfmt emulation support
    Signed-off-by: Ville Ilvonen <vilvo@iki.fi>

--- hosts/nixos/carrie/configuration.nix
+++ hosts/nixos/carrie/configuration.nix
@@ -9,6 +9,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
   networking.hostName = "carrie";
   networking.networkmanager.enable = true;
